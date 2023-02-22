b03497ca9a92cf3b72950fb22afd4690c0d9e4c1
Ismaeel Mughal <ismaeel.mughal.1912@gmail.com>
2023-02-22 19:43:46 +0500

    Insertion

--- Trees/BinaryTree.cpp
+++ Trees/BinaryTree.cpp
@@ -96,6 +96,40 @@ int height(struct Node* node) {
 	}
 }
 
+Node* InsertNode(Node* root, int data)
+{
+	// If the tree is empty, assign new node address to root
+	if (root == NULL) {
+		root = new Node(data);
+		return root;
+	}
+
+	// Else, do level order traversal until we find an empty
+	// place, i.e. either left child or right child of some
+	// node is pointing to NULL.
+	queue<Node*> q;
+	q.push(root);
+
+	while (!q.empty()) {
+		Node* temp = q.front();
+		q.pop();
+
+		if (temp->left != NULL)
+			q.push(temp->left);
+		else {
+			temp->left = new Node(data);
+			return root;
+		}
+
+		if (temp->right != NULL)
+			q.push(temp->right);
+		else {
+			temp->right = new Node(data);
+			return root;
+		}
+	}
+}
+
 int main()
 {
 	struct Node* root = new Node(1);
